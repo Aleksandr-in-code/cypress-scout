9d4f8442d711b273272e92dff6952b3bac17dc2e
Aleksander <aleksandkurnev@yandex.ru>
2024-08-28 16:16:36 +0300

    test: improved locators

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,4 @@
-// Cypress._.times(20, () => {
+
 describe('E2E Tests of demoblaze.com', () => {
 
 const signUsername = ("Tesg" + Date.now());
@@ -6,41 +6,44 @@ const signPassword = "Qwerty123";
 
     it('As a user, purchase a monitor without authorization and registration', () => {
         cy.visit('https://www.demoblaze.com');
-        cy.get('[onclick="byCat(\'monitor\')"]').click();
-        cy.get(':nth-child(1) > .card > .card-block > .card-title > .hrefch').click();
-        cy.get('.col-sm-12 > .btn').click();
+        cy.contains('Monitors').click();
+        cy.contains('Apple monitor 24').click();
+        cy.get('[onclick="addToCart(10)"]').click();
         cy.get('#cartur').click();
-        cy.get('.col-lg-1 > .btn').click();
+        cy.contains('Apple monitor 24').should('be.visible');
+        cy.contains('Place Order').click();
         cy.get('#name').invoke('val', "Jimmy");
         cy.get('#country').invoke('val', "Canada");
         cy.get('#city').invoke('val', "Edmonton");
         cy.get('#card').invoke('val', "4111 1111 1111 1111");
         cy.get('#month').invoke('val', "09");
         cy.get('#year').invoke('val', "2024");
-        cy.get('#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
-      
+        cy.contains('Purchase').click();        
       });
-  
+    
     it('As a user, Purchase a laptop through registration', () => {
       cy.visit('https://www.demoblaze.com');
       cy.get('#signin2').click();
       cy.get('#sign-username').invoke('val', signUsername);
       cy.get('#sign-password').invoke('val', signPassword);
       cy.wait(2000);
-      cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
-      cy.get('[onclick="byCat(\'notebook\')"]').click();
-      cy.get(':nth-child(1) > .card > .card-block > .card-title > .hrefch').click();
-      cy.get('.col-sm-12 > .btn').click();
+      cy.get('[onclick="register()"]').click();
+      cy.wait(2000);
+      cy.contains('Laptops').click();
+      cy.contains('Sony vaio i5').click();
+      cy.get('[onclick="addToCart(8)"]').click()
       cy.get('#cartur').click();
-      cy.get('.col-lg-1 > .btn').click();
+      cy.contains('Sony vaio i5').should('be.visible');
+      cy.contains('Place Order').click();
       cy.get('#name').invoke('val', "Jimmy");
       cy.get('#country').invoke('val', "Canada");
       cy.get('#city').invoke('val', "Edmonton");
       cy.get('#card').invoke('val', "4111 1111 1111 1111");
       cy.get('#month').invoke('val', "09");
       cy.get('#year').invoke('val', "2024");
-      cy.get('#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
+      cy.contains('Purchase').click();
     });
+
 
     it('As a user, Purchase a phone through authorization', () => {
       cy.visit('https://www.demoblaze.com');
@@ -48,18 +51,18 @@ const signPassword = "Qwerty123";
       cy.get('#loginusername').invoke('val', signUsername);
       cy.get('#loginpassword').invoke('val', signPassword);
       cy.wait(2000);
-      cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
-      cy.get(':nth-child(1) > .card > .card-block > .card-title > .hrefch').click();
-      cy.get('.col-sm-12 > .btn').click();
+      cy.get('[onclick="logIn()"]').click();
+      cy.contains('Samsung galaxy s6').click();
+      cy.get('[onclick="addToCart(1)"]').click();
       cy.get('#cartur').click();
-      cy.get('.col-lg-1 > .btn').click();
+      cy.contains('Samsung galaxy s6').should('be.visible');
+      cy.contains('Place Order').click();
       cy.get('#name').invoke('val', "Jimmy");
       cy.get('#country').invoke('val', "Canada");
       cy.get('#city').invoke('val', "Edmonton");
       cy.get('#card').invoke('val', "4111 1111 1111 1111");
       cy.get('#month').invoke('val', "09");
       cy.get('#year').invoke('val', "2024");
-      cy.get('#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
+      cy.contains('Purchase').click();
     });
-  });
-// });
+});
