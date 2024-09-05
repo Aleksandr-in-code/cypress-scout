ae475f26b2d11158dae60fac15fb97c3c760f0e8
Aleksander <aleksandkurnev@yandex.ru>
2024-09-05 12:45:41 +0300

    test: added command SignUp and login

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,6 +3,9 @@ describe('E2E Tests of demoblaze.com', () => {
 
   const signUsername = ("Tesg" + Date.now());
   const signPassword = "Qwerty123";
+  const monitorName = 'Apple monitor 24';
+  const laptopName = 'Sony vaio i5';
+  const phoneName = 'Samsung galaxy s6';  
 
   beforeEach(() => {
     cy.visit('https://www.demoblaze.com/');
@@ -10,10 +13,10 @@ describe('E2E Tests of demoblaze.com', () => {
 
     it('As a user, purchase a monitor without authorization and registration', () => {
         cy.contains('#itemc', 'Monitors').click();
-        cy.contains('.hrefch', 'Apple monitor 24').click();
+        cy.contains('.hrefch', monitorName).click();
         cy.contains('a.btn', 'Add to cart').click();
         cy.get('#cartur').click();
-        cy.contains('.success', 'Apple monitor 24').should('be.visible');
+        cy.contains('.success', monitorName).should('be.visible');
         cy.contains('.btn.btn-success', 'Place Order').click();
         cy.get('#name').invoke('val', "Jimmy");
         cy.get('#country').invoke('val', "Canada");
@@ -25,17 +28,12 @@ describe('E2E Tests of demoblaze.com', () => {
       });
     
     it('As a user, Purchase a laptop through registration', () => {
-      cy.get('#signin2').click();
-      cy.get('#sign-username').invoke('val', signUsername);
-      cy.get('#sign-password').invoke('val', signPassword);
-      cy.wait(2000);
-      cy.contains('.btn.btn-primary', 'Sign up').click();
-      cy.wait(2000);
+      cy.signUp(signUsername, signPassword)
       cy.contains('#itemc', 'Laptops').click();
-      cy.contains('.hrefch', 'Sony vaio i5').click();
+      cy.contains('.hrefch', laptopName).click();
       cy.contains('a.btn', 'Add to cart').click();
       cy.get('#cartur').click();
-      cy.contains('.success', 'Sony vaio i5').should('be.visible');
+      cy.contains('.success', laptopName).should('be.visible');
       cy.contains('.btn.btn-success', 'Place Order').click();
       cy.get('#name').invoke('val', "Jimmy");
       cy.get('#country').invoke('val', "Canada");
@@ -48,16 +46,11 @@ describe('E2E Tests of demoblaze.com', () => {
 
 
     it('As a user, Purchase a phone through authorization', () => {
-      cy.get('#login2').click();
-      cy.get('#loginusername').invoke('val', signUsername);
-      cy.get('#loginpassword').invoke('val', signPassword);
-      cy.wait(2000);
-      cy.contains('.btn.btn-primary', 'Log in').click();
-      cy.wait(2000);
-      cy.contains('.hrefch', 'Samsung galaxy s6').click();
+      cy.login(signUsername, signPassword)
+      cy.contains('.hrefch', phoneName).click();
       cy.contains('a.btn', 'Add to cart').click();
       cy.get('#cartur').click();
-      cy.contains('.success', 'Samsung galaxy s6').should('be.visible');
+      cy.contains('.success', phoneName).should('be.visible');
       cy.contains('.btn.btn-success', 'Place Order').click();
       cy.get('#name').invoke('val', "Jimmy");
       cy.get('#country').invoke('val', "Canada");
