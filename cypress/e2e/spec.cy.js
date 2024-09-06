d93fabdd028ac27ba9eaa1429060073aad023a62
Aleksander <aleksandkurnev@yandex.ru>
2024-09-06 10:23:04 +0300

    test: added basic checks

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,27 +5,30 @@ describe('E2E Tests of demoblaze.com', () => {
   const signPassword = "Qwerty123";
   const monitorName = 'Apple monitor 24';
   const laptopName = 'Sony vaio i5';
-  const phoneName = 'Samsung galaxy s6';  
+  const phoneName = 'Samsung galaxy s6';
 
   beforeEach(() => {
     cy.visit('https://www.demoblaze.com/');
+    cy.intercept('Post', '/deletecart').as('deletecart');
   });
 
     it('As a user, purchase a monitor without authorization and registration', () => {
-        cy.contains('#itemc', 'Monitors').click();
-        cy.contains('.hrefch', monitorName).click();
-        cy.contains('a.btn', 'Add to cart').click();
-        cy.get('#cartur').click();
-        cy.contains('.success', monitorName).should('be.visible');
-        cy.contains('.btn.btn-success', 'Place Order').click();
-        cy.get('#name').invoke('val', "Jimmy");
-        cy.get('#country').invoke('val', "Canada");
-        cy.get('#city').invoke('val', "Edmonton");
-        cy.get('#card').invoke('val', "4111 1111 1111 1111");
-        cy.get('#month').invoke('val', "09");
-        cy.get('#year').invoke('val', "2024");
-        cy.contains('.btn.btn-primary', 'Purchase').click();        
-      });
+      cy.contains('#itemc', 'Monitors').click();
+      cy.contains('.hrefch', monitorName).should('be.visible').click();
+      cy.contains('a.btn', 'Add to cart').click();
+      cy.get('#cartur').click();
+      cy.contains('.success', monitorName).should('be.visible');
+      cy.contains('.btn.btn-success', 'Place Order').click();
+      cy.get('#name').invoke('val', "Jimmy");
+      cy.get('#country').invoke('val', "Canada");
+      cy.get('#city').invoke('val', "Edmonton");
+      cy.get('#card').invoke('val', "4111 1111 1111 1111");
+      cy.get('#month').invoke('val', "09");
+      cy.get('#year').invoke('val', "2024");
+      cy.contains('.btn.btn-primary', 'Purchase').click();
+      cy.contains('.sweet-alert', 'Thank you for your purchase!').should('be.visible');
+      cy.wait('@deletecart');
+    });
     
     it('As a user, Purchase a laptop through registration', () => {
       cy.signUp(signUsername, signPassword)
@@ -41,7 +44,9 @@ describe('E2E Tests of demoblaze.com', () => {
       cy.get('#card').invoke('val', "4111 1111 1111 1111");
       cy.get('#month').invoke('val', "09");
       cy.get('#year').invoke('val', "2024");
-      cy.contains('.btn.btn-primary', 'Purchase').click(); 
+      cy.contains('.btn.btn-primary', 'Purchase').click();
+      cy.contains('.sweet-alert', 'Thank you for your purchase!').should('be.visible');
+      cy.wait('@deletecart');
     });
 
 
@@ -59,5 +64,7 @@ describe('E2E Tests of demoblaze.com', () => {
       cy.get('#month').invoke('val', "09");
       cy.get('#year').invoke('val', "2024");
       cy.contains('.btn.btn-primary', 'Purchase').click();
+      cy.contains('.sweet-alert', 'Thank you for your purchase!').should('be.visible');
+      cy.wait('@deletecart');
     });
 });
