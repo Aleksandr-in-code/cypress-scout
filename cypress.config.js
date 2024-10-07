2c8463e6cc1b9c2704fcba205fc5ed8dbd47ce31
Aleksander <aleksandkurnev@yandex.ru>
2024-10-07 13:12:57 +0300

    test: improve code

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
+
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://www.demoblaze.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },
-});
+    }
+  }
+})
